2f5716c5da35f751ddf41f6ee49e412df43c69e3
Samantha Caangay <samanthacaangay21@gmail.com>
2026-06-18 00:42:05 +0800

    now we have many messages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
+Console.WriteLine("Hello, pink ba mama mo, nugagawen?");
